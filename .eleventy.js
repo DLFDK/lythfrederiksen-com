dcd764a166f396350b374d3ec6eedf68ca9446b5
DLFDK <dennislythfrederiksen@gmail.com>
2021-10-13 06:43:23 +0200

    Make browsersync open a window at launch

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,9 @@
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/css/");
     eleventyConfig.addWatchTarget("./src/css/");
+    eleventyConfig.setBrowserSyncConfig({
+        open: true,
+      });
     return {
         dir: {
             input: "src",
