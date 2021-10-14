eb7c01f5458707fc2b60956c5417418b26e4faf7
DLFDK <dennislythfrederiksen@gmail.com>
2021-10-14 17:52:36 +0200

    Change to only use browser-sync proxy when --serve flag set

--- .eleventy.js
+++ .eleventy.js
@@ -1,20 +1,23 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
-const proxy_dmi = createProxyMiddleware('/images', {
-    target: 'https://res.cloudinary.com/dypwsyigo/image/upload/lythfrederiksen-com',
-    changeOrigin: true,
-    pathRewrite: {
-        '^/images*': '/' // remove base path
-    }
-});
-
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/images/");
     eleventyConfig.addPassthroughCopy("./src/css/");
     eleventyConfig.addWatchTarget("./src/css/");
-    eleventyConfig.setBrowserSyncConfig({
-        open: true,
-        middleware: [proxy_dmi]
-    });
+
+    if (process.argv.includes('--serve')) {
+        const { createProxyMiddleware } = require('http-proxy-middleware');
+        const proxy_dmi = createProxyMiddleware('/images', {
+            target: 'https://res.cloudinary.com/dypwsyigo/image/upload/lythfrederiksen-com',
+            changeOrigin: true,
+            pathRewrite: {
+                '^/images*': '/' // remove base path
+            }
+        });
+        eleventyConfig.setBrowserSyncConfig({
+            open: true,
+            middleware: [proxy_dmi]
+        });
+    }
+
     return {
         dir: {
             input: "src",
