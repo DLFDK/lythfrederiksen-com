e4ac65150bb887f02695099241e2247082dcec6e
DLFDK <dennislythfrederiksen@gmail.com>
2021-10-23 22:50:39 +0200

    Add sass support

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,12 @@
 const htmlmin = require("html-minifier");
+const sass = require("sass");
+const fs = require("fs");
 
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("./src/css/");
     eleventyConfig.addWatchTarget("./src/css/");
 
     eleventyConfig.addTransform("htmlmin", (content, outputPath) => {
-        if ( outputPath && outputPath.endsWith("html")){
+        if (outputPath && outputPath.endsWith("html")) {
             let minified = htmlmin.minify(content, {
                 removeComments: true
             });
@@ -15,28 +16,56 @@ module.exports = function (eleventyConfig) {
         }
     });
 
-    
-
-    if (process.argv.includes('--serve')) {
-        const { createProxyMiddleware } = require('http-proxy-middleware');
-        const proxy = createProxyMiddleware('/cloudinary', {
-            target: 'http://localhost:8080/images/',
-            changeOrigin: true,
-            pathRewrite: function(path, req){
-                return path.slice(path.lastIndexOf("/"));
-            }
-        });
-        eleventyConfig.setBrowserSyncConfig({
-            // open: true,
-            middleware: [proxy],
-            server: {
-                baseDir: "./dist",
-                routes: {
-                    "/images": "./images"
+    eleventyConfig.addTransform("css", (content, outputPath) => {
+        if (outputPath && outputPath.endsWith("html")) {
+            const styleLinks = content.matchAll(/<link.*rel="stylesheet".*>/g);
+            if(styleLinks){
+                let output = content;
+                for (const [string] of styleLinks) {
+                    const [, path, file, extension] = string.match(/href="[\.\/]*(.*\/)(.*)\.(s?[ac]ss)"/);
+                    try {
+                        const result = sass.renderSync({
+                            file: `src/${path}${file}.${extension}`
+                        });
+                        if (string.includes("inline")) {
+                            output = output.replace(string, `<style>${result.css}</style>`)
+                        } else {
+                            fs.mkdirSync(`dist/${path}`, { recursive: true })
+                            fs.writeFileSync(`dist/${path}${file}.css`, result.css);
+                            output = output.replace(string, `<link rel="stylesheet" href="/${path}${file}.css">`)
+                        }
+                    } catch (error) {
+                        console.log(error);
+                    }
                 }
+                return output;
+            } else {
+                return content //File does not have a stylesheet link
             }
-        });
-    }
+        }
+        return content; //File is not .html
+    });
+
+    // if (process.argv.includes('--serve')) {
+    //     const { createProxyMiddleware } = require('http-proxy-middleware');
+    //     const proxy = createProxyMiddleware('/cloudinary', {
+    //         target: 'http://localhost:8080/images/',
+    //         changeOrigin: true,
+    //         pathRewrite: function(path, req){
+    //             return path.slice(path.lastIndexOf("/"));
+    //         }
+    //     });
+    //     eleventyConfig.setBrowserSyncConfig({
+    //         open: true,
+    //         middleware: [proxy],
+    //         server: {
+    //             baseDir: "./dist",
+    //             routes: {
+    //                 "/images": "./images"
+    //             }
+    //         }
+    //     });
+    // }
 
     return {
         dir: {
--- src/blog/posts/first-post.md
+++ src/blog/posts/first-post.md
@@ -1,5 +1,5 @@
 ---
 title: Turning the generator to Eleventy
 ---
-
+# Hello
 This is the very first post.
