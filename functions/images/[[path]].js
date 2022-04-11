7ef88fa524edf666dfd96ab0e89ba5937e2c8f04
DLFDK <dennislythfrederiksen@gmail.com>
2022-04-11 20:59:24 +0200

    Add back proper functionality

--- functions/images/[[path]].js
+++ functions/images/[[path]].js
@@ -11,28 +11,8 @@
 //     return fetch(imageRequest);
 // }
 
-// export async function onRequest(context) {
-// // // Contents of context object
-// const {
-//     request, // same as existing Worker API
-//     env, // same as existing Worker API
-//     params, // if filename includes [id] or [[path]]
-//     waitUntil, // same as ctx.waitUntil in existing Worker API
-//     next, // used for middleware or to fetch assets
-//     data, // arbitrary space for passing data between middlewares
-// } = context;
-
-// // return new Response('Hello, world!');
-// const regex = /.*images\/(.*)/;
-// const url = request.url.match(regex)[1];
-// const imageRequest = new Request(url, {
-//     headers: request.headers
-// });
-// return fetch(imageRequest);
-// }
-
 export async function onRequest(context) {
-    // Contents of context object
+    // // Contents of context object
     const {
         request, // same as existing Worker API
         env, // same as existing Worker API
@@ -42,6 +22,26 @@ export async function onRequest(context) {
         data, // arbitrary space for passing data between middlewares
     } = context;
 
-    return new Response('Hello, world!');
+    // return new Response('Hello, world!');
+    const regex = /.*images\/(.*)/;
+    const url = request.url.match(regex)[1];
+    const imageRequest = new Request(url, {
+        headers: request.headers
+    });
+    return fetch(imageRequest);
 }
+
+// export async function onRequest(context) {
+//     // Contents of context object
+//     const {
+//         request, // same as existing Worker API
+//         env, // same as existing Worker API
+//         params, // if filename includes [id] or [[path]]
+//         waitUntil, // same as ctx.waitUntil in existing Worker API
+//         next, // used for middleware or to fetch assets
+//         data, // arbitrary space for passing data between middlewares
+//     } = context;
+
+//     return new Response('Hello, world!');
+// }
 
