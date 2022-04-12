69d98b4160fe081078b98d3a37c20d45edd01bf8
DLFDK <dennislythfrederiksen@gmail.com>
2022-04-12 02:22:30 +0200

    Remove comments

--- functions/images/[[path]].js
+++ functions/images/[[path]].js
@@ -1,28 +1,5 @@
-// addEventListener("fetch", event => {
-//     event.respondWith(handleRequest(event.request))
-// })
-
-// async function handleRequest(request) {
-//     const regex = /.*images\/(.*)/;
-//     const url = request.url.match(regex)[1];
-//     const imageRequest = new Request(url, {
-//         headers: request.headers
-//     });
-//     return fetch(imageRequest);
-// }
-
 export async function onRequest(context) {
-    // // Contents of context object
-    const {
-        request, // same as existing Worker API
-        env, // same as existing Worker API
-        params, // if filename includes [id] or [[path]]
-        waitUntil, // same as ctx.waitUntil in existing Worker API
-        next, // used for middleware or to fetch assets
-        data, // arbitrary space for passing data between middlewares
-    } = context;
-
-    // return new Response('Hello, world!');
+    const { request } = context;
     const regex = /.*images\/(.*)/;
     const url = request.url.match(regex)[1];
     const imageRequest = new Request(url, {
@@ -30,18 +7,3 @@ export async function onRequest(context) {
     });
     return fetch(imageRequest);
 }
-
-// export async function onRequest(context) {
-//     // Contents of context object
-//     const {
-//         request, // same as existing Worker API
-//         env, // same as existing Worker API
-//         params, // if filename includes [id] or [[path]]
-//         waitUntil, // same as ctx.waitUntil in existing Worker API
-//         next, // used for middleware or to fetch assets
-//         data, // arbitrary space for passing data between middlewares
-//     } = context;
-
-//     return new Response('Hello, world!');
-// }
-
