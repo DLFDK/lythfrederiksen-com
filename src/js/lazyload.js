5cd2ba05dc279743c5997c60f1c82c1472fb490d
DLFDK <dennislythfrederiksen@gmail.com>
2022-04-12 02:53:27 +0200

    Add support for devicePixelRatio

--- src/js/lazyload.js
+++ src/js/lazyload.js
@@ -4,9 +4,11 @@ function lazyload() {
     const observer = new IntersectionObserver((entries, observer) => {
         for (const entry of entries) {
             if (entry.isIntersecting) {
-                entry.target.style.height = `${entry.target.offsetHeight} + px`;
-                entry.target.style.width = `${entry.target.offsetWidth} + px`;
-                entry.target.src = entry.target.dataset.src.replace("{width}", entry.target.offsetWidth).replace("{height}", entry.target.offsetHeight);
+                entry.target.style.height = `${entry.target.offsetHeight}px`;
+                entry.target.style.width = `${entry.target.offsetWidth}px`;
+                const pixelWidth = entry.target.offsetWidth * window.devicePixelRatio.toFixed(0);
+                const pixelHeight = entry.target.offsetHeight * window.devicePixelRatio.toFixed(0);
+                entry.target.src = entry.target.dataset.src.replace("{width}", pixelWidth).replace("{height}", pixelHeight);
                 observer.unobserve(entry.target);
             }
         }
