2f1cddcd3cef82cadd54b65a38ae0ad47c84a56e
DLFDK <dennislythfrederiksen@gmail.com>
2022-04-12 02:38:34 +0200

    Round width and height of lazy loaded images

--- src/js/lazyload.js
+++ src/js/lazyload.js
@@ -4,6 +4,8 @@ function lazyload() {
     const observer = new IntersectionObserver((entries, observer) => {
         for (const entry of entries) {
             if (entry.isIntersecting) {
+                entry.target.style.height = `${entry.target.offsetHeight} + px`;
+                entry.target.style.width = `${entry.target.offsetWidth} + px`;
                 entry.target.src = entry.target.dataset.src.replace("{width}", entry.target.offsetWidth).replace("{height}", entry.target.offsetHeight);
                 observer.unobserve(entry.target);
             }
