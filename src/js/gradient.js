4212d477bd1eeaf8cb9e04f350210f41c898560e
DLFDK <dennislythfrederiksen@gmail.com>
2022-04-28 03:01:29 +0200

    Test canvas gradient

--- src/js/gradient.js
+++ src/js/gradient.js
@@ -3,61 +3,86 @@ function gradient() {
     const container = document.getElementById("hero__background");
     const canvas = document.getElementById("hero__canvas");
     const ctx = canvas.getContext("2d");
+    canvas.style.height = container.offsetHeight + "px";
     const width = canvas.offsetWidth;
     const height = width;
-    // const width = 400;
-    // const height = 400;
     canvas.width = width;
     canvas.height = height;
 
     const halfWidth = width / 2;
     const halfHeight = height / 2;
 
+    const gradientColorZero = "#a960ee";
+    const gradientColorOne = "#ff333d";
+    const gradientColorTwo = "#90e0ff";
+    const gradientColorThree = "#ffcb57";
+    const gradientColorZeroTransparent = "rgba(169, 96, 238, 0)";
+    const gradientColorOneTransparent = "rgba(255, 51, 61, 0)";
+    const gradientColorTwoTransparent = "rgba(144, 224, 255, 0)";
+    const gradientColorThreeTransparent = "rgba(255, 203, 87, 0)";
+
+    ctx.fillStyle = gradientColorZero;
+    ctx.fillRect(0, 0, width, height);
+
     createGradient([
-        ["#ffcb57", 0.23],
-        ["#ffcb5700", 0.67],
-        ["#ffcb5700", 0.99],
-        ["black", 0.99],
-    ], 385, -24);
-
+        [gradientColorTwo, 0.25],
+        [gradientColorTwoTransparent, 0.50],
+    ], 520, -250 * 2.5);
+    createGradient([
+        [gradientColorTwo, 0.15],
+        [gradientColorTwoTransparent, 0.50],
+    ], -620, 0 * 2.5);
+    createGradient([
+        [gradientColorZero, 0.10],
+        [gradientColorZeroTransparent, 0.60],
+    ], -420, 120 * 2.5);
+    createGradient([
+        [gradientColorZero, 0.30],
+        [gradientColorZeroTransparent, 0.67],
+    ], 122, -120 * 2.5);
+    createGradient([
+        [gradientColorOne, 0.4],
+        [gradientColorOneTransparent, 0.57],
+    ], 495, -44 * 2.5);
+    createGradient([
+        [gradientColorTwo, 0.1],
+        [gradientColorTwoTransparent, 0.60],
+    ], -120, 250 * 2.5);
+    createGradient([
+        [gradientColorOne, 0],
+        [gradientColorOneTransparent, 0.60],
+    ], -940, 290 * 2.5);
+    createGradient([
+        [gradientColorThree, 0.23],
+        [gradientColorThreeTransparent, 0.67],
+    ], 385, -24 * 2.5);
+    
+    
     // createGradient([
-    //     ["#ffcb57", 0.23],
-    //     ["#ffcb5700", 0.67],
-    //     ["#ffcb5700", 0.99],
+    //     [gradientColorThree, 0.23],
+    //     [gradientColorThreeTransparent, 0.67],
+    //     [gradientColorThreeTransparent, 0.99],
     //     ["black", 0.99],
-    // ], 0, 0);
-
-
-    // const grad1 = ctx.createRadialGradient(0, 400, 0, 0, 400, 2 * width);
-    // const grad1 = ctx.createRadialGradient(halfWidth,halfHeight,0, halfWidth,halfHeight,halfWidth);
-    // grad1.addColorStop(0, 'pink');
-    // grad1.addColorStop(0.9, 'white');
-    // grad1.addColorStop(0.9, 'green');
-    // ctx.fillStyle = grad1;
-    // ctx.fillRect(0, 0, width, height);
-
-    // const grad2 = ctx.createRadialGradient(width * 0.5, 0.5 * height, 0, 0.5 * width, 0.5 * height, height);
-    // grad2.addColorStop(0, '#a960ee');
-    // grad2.addColorStop(0.4, '#a960ee00');
-    // ctx.fillStyle = grad2;
-    // ctx.fillRect(0, 0, width, height);
+    // ], 385, -100 * 2.5);
 
     function createGradient(colorStops, positionX, positionY) {
         const centerX = halfWidth + positionX;
         const centerY = halfHeight + positionY;
 
+        const signX = Math.sign(positionX);
+        const signY = Math.sign(positionY);
+
         const centers = [];
         centers.push([centerX, centerY]);
         if (positionX && positionY) {
-            centers.push([centerX - Math.sign(positionX) * width, centerY - Math.sign(positionY) * height]);
-            centers.push([centerX - Math.sign(positionX) * width, centerY]);
-            centers.push([centerX, centerY - Math.sign(positionY) * height]);
+            centers.push([centerX - signX * width, centerY - signY * height]);
+            centers.push([centerX - signX * width, centerY]);
+            centers.push([centerX, centerY - signY * height]);
         } else if (positionX) {
-            centers.push([centerX - Math.sign(positionX) * width, centerY]);
+            centers.push([centerX - signX * width, centerY]);
         } else if (positionY) {
-            centers.push([centerX, centerY - Math.sign(positionY)] * height);
+            centers.push([centerX, centerY - signY] * height);
         }
-
 
         for (const [x, y] of centers) {
             const grad = ctx.createRadialGradient(x, y, 0, x, y, halfWidth * Math.SQRT2);
@@ -65,18 +90,7 @@ function gradient() {
                 grad.addColorStop(stop, color);
             }
             ctx.fillStyle = grad;
-            console.log(x, y);
             ctx.fillRect(x - halfWidth, y - halfHeight, width, height);
         }
-
-        // const adjPercOne = Math.sin(45) * fractionOne;
-        // const adjPercTwo = Math.sin(45) * fractionTwo;
-        // const adjPercThree = Math.sin(45) * fractionThree;
-
-
-        // grad.addColorStop(fractionTwo, colorTwo);
-        // grad.addColorStop(fractionThree, colorThree);
-        // ctx.fillRect(0, 0, width, height);
-        // ctx.fillRect(positionX - halfWidth, positionY - halfWidth, width, height);
     }
 }
