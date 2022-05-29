660236622d4eebd9a3be7c0072e1db62745ae124
DLFDK <dennislythfrederiksen@gmail.com>
2022-05-29 05:20:30 +0200

    Implement pagination of category sub-pages

---
+++ src/_data/categories.js
@@ -0,0 +1,9 @@
+const rootsAndTypes = [["Blog", "posts"], ["Projects", "projects"]];
+const categories = ["Build", "Write", "Code"];
+const combination = [];
+for(const item of rootsAndTypes){
+    for(const category of categories){
+        combination.push([...item, category])
+    }
+}
+module.exports = combination;
