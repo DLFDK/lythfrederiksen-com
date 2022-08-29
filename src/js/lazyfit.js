9151b40d0a89ca13c3578518db792b404addc6d8
DLFDK <dennislythfrederiksen@gmail.com>
2022-08-30 00:42:25 +0200

    Revert to before tasks

--- src/js/lazyfit.js
+++ src/js/lazyfit.js
@@ -1,66 +1,39 @@
-requestIdleCallback(lazyfit);
-async function lazyfit() {
+lazyfit();
+function lazyfit() {
     const halfWindowHeight = window.innerHeight / 2;
-
-    const observer = new IntersectionObserver((entries) => {
+    const images = [...document.getElementsByClassName("lazyfit")];
+    const observer = new IntersectionObserver((entries, observer) => {
         for (const entry of entries) {
-            setTimeout(() => {
-                intersectionTest(entry)
-            }, 0)
+            if (entry.isIntersecting) {
+                entry.target.style.height = `${entry.target.offsetHeight}px`;
+                entry.target.style.width = `${entry.target.offsetWidth}px`;
+                const pixelWidth = entry.target.offsetWidth * window.devicePixelRatio.toFixed(0);
+                const pixelHeight = entry.target.offsetHeight * window.devicePixelRatio.toFixed(0);
+                entry.target.src = entry.target.dataset.src.replace("{width}", pixelWidth).replace("{height}", pixelHeight);
+                observer.unobserve(entry.target);
+                entry.target.addEventListener("load", () => {
+                    if (entry.target.dataset.addClass) {
+                        for (const className of entry.target.dataset.addClass.split(" ")) {
+                            entry.target.classList.add(className);
+                        }
+                    }
+                    if (entry.target.dataset.removeClass) {
+                        for (const className of entry.target.dataset.removeClass.split(" ")) {
+                            entry.target.classList.remove(className);
+                        }
+                    }
+                    if (entry.target.dataset.toggleClass) {
+                        for (const className of entry.target.dataset.toggleClass.split(" ")) {
+                            entry.target.classList.toggle(className);
+                        }
+                    }
+                });
+            }
         }
     }, {
         rootMargin: `${halfWindowHeight}px 0px ${halfWindowHeight}px 0px`
     });
-
-    setTimeout(getImages, 0);
-
-    function getImages() {
-        const images = [...document.getElementsByClassName("lazyfit")];
-        setTimeout(() => {
-            observeImages(images, 0, images.length - 1)
-        }, 0);
-    }
-    
-    function observeImages(images, index, end) {
-        observer.observe(images[index]);
-        if(index < end){
-            setTimeout(() => {
-                observeImages(images, index + 1, end)
-            }, 0);
-        }
-    }
-
-    function intersectionTest(entry) {
-        if (entry.isIntersecting) {
-            observer.unobserve(entry.target);
-            setTimeout(() => {
-                loadImage(entry.target);
-            }, 0);
-        }
-    }
-
-    function loadImage(image) {
-        // image.style.height = `${image.offsetHeight}px`;
-        // image.style.width = `${image.offsetWidth}px`;
-        const pixelWidth = image.offsetWidth * window.devicePixelRatio.toFixed(0);//Should the rounding happen after multiplication?
-        const pixelHeight = image.offsetHeight * window.devicePixelRatio.toFixed(0);
-        image.src = image.dataset.src.replace("{width}", pixelWidth).replace("{height}", pixelHeight);
-        image.addEventListener("load", () => {
-            if (image.dataset.addClass) {
-                for (const className of image.dataset.addClass.split(" ")) {
-                    image.classList.add(className);
-                }
-            }
-            if (image.dataset.removeClass) {
-                for (const className of image.dataset.removeClass.split(" ")) {
-                    image.classList.remove(className);
-                }
-            }
-            if (image.dataset.toggleClass) {
-                for (const className of image.dataset.toggleClass.split(" ")) {
-                    image.classList.toggle(className);
-                }
-            }
-        });
+    for (const image of images) {
+        observer.observe(image);
     }
 }
