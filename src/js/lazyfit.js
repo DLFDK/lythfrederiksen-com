5df1489e4c12483ddafb1999e4f4002101dfe424
DLFDK <dennislythfrederiksen@gmail.com>
2022-08-29 06:25:37 +0200

    Remove commented code

--- src/js/lazyfit.js
+++ src/js/lazyfit.js
@@ -1,72 +1,6 @@
 lazyfit();
 async function lazyfit() {
-    // const yieldToEventLoop = () => new Promise((resolve) => setTimeout(() => { 
-    //     console.log("Yielding");
-    //     resolve() 
-    // }, 0))
-
-    // const getInnerHeight = "getInnerHeight";
-    // const setupObserver = "setupObserver";
-    // const getImages = "getImages";
-    // const observeImages = "observeImages";
-
-    // const startMarker = "startMarker";
-    // performance.mark(startMarker);
-
-    const maxLoopTime = 0.1;
-
     const halfWindowHeight = window.innerHeight / 2;
-    // await yieldToEventLoop();
-
-    // performance.mark(getInnerHeight);
-
-    // const observer = new IntersectionObserver((entries, observer) => {
-    //     for (const entry of entries) {
-    //         console.log(entry.isIntersecting && false);
-    //         if (entry.isIntersecting) {
-    //             // entry.target.style.height = `${entry.target.offsetHeight}px`;
-    //             // entry.target.style.width = `${entry.target.offsetWidth}px`;
-    //             const pixelWidth = entry.target.offsetWidth * window.devicePixelRatio.toFixed(0);//Should the rounding happen after multiplication?
-    //             const pixelHeight = entry.target.offsetHeight * window.devicePixelRatio.toFixed(0);
-    //             entry.target.src = entry.target.dataset.src.replace("{width}", pixelWidth).replace("{height}", pixelHeight);
-    //             observer.unobserve(entry.target);
-    //             entry.target.addEventListener("load", () => {
-    //                 if (entry.target.dataset.addClass) {
-    //                     for (const className of entry.target.dataset.addClass.split(" ")) {
-    //                         entry.target.classList.add(className);
-    //                     }
-    //                 }
-    //                 if (entry.target.dataset.removeClass) {
-    //                     for (const className of entry.target.dataset.removeClass.split(" ")) {
-    //                         entry.target.classList.remove(className);
-    //                     }
-    //                 }
-    //                 if (entry.target.dataset.toggleClass) {
-    //                     for (const className of entry.target.dataset.toggleClass.split(" ")) {
-    //                         entry.target.classList.toggle(className);
-    //                     }
-    //                 }
-    //             });
-    //         }
-    //     }
-    // }, {
-    //     rootMargin: `${halfWindowHeight}px 0px ${halfWindowHeight}px 0px`
-    // });
-
-    // const observer = new IntersectionObserver((entries, observer) => {
-    //     console.time("Observer");
-    //     for (const entry of entries) {
-    //         if (entry.isIntersecting) {
-    //             observer.unobserve(entry.target);
-    //             setTimeout(() => {
-    //                 loadImage(entry.target);
-    //             }, 0);
-    //         }
-    //     }
-    //     console.timeEnd("Observer");
-    // }, {
-    //     rootMargin: `${halfWindowHeight}px 0px ${halfWindowHeight}px 0px`
-    // });
 
     const observer = new IntersectionObserver((entries) => {
         for (const entry of entries) {
@@ -79,32 +13,6 @@ async function lazyfit() {
     });
 
     setTimeout(getImages, 0);
-    // await yieldToEventLoop();
-
-    // performance.mark(setupObserver);
-
-    // const images = [...document.getElementsByClassName("lazyfit")];
-    // await yieldToEventLoop();
-
-    // performance.mark(getImages);
-
-    // for (const image of images) {
-    // console.time("Loop");
-    // observer.observe(image);
-    // await yieldToEventLoop();
-    // console.timeEnd("Loop");
-    // }
-    // performance.mark(observeImages);
-
-
-    // performance.measure("getInnerHeight", startMarker, getInnerHeight);
-    // performance.measure("setupObserver", getInnerHeight, setupObserver);
-    // performance.measure("getImages", setupObserver, getImages);
-    // performance.measure("observeImages", getImages, observeImages);
-    // console.log(performance.getEntriesByType("measure")[0].duration);
-    // for (const entry of performance.getEntriesByType("measure")) {
-    //     console.log(entry.name, entry.duration);
-    // }
 
     function getImages() {
         const images = [...document.getElementsByClassName("lazyfit")];
@@ -120,16 +28,6 @@ async function lazyfit() {
                 observeImages(images, index + 1, end)
             }, 0);
         }
-        // for (const image of images) {
-        //     observer.observe(image);
-        //     images.delete(image);
-        //     if (performance.now() - startTime > maxLoopTime && images.size) {
-        //         setTimeout(() => {
-        //             observeImages(images)
-        //         }, 0);
-        //         break;
-        //     }
-        // }
     }
 
     function intersectionTest(entry) {
