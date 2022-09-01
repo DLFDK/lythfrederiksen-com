25a86f67c96fd227f807c03ed587944202cdb150
DLFDK <dennislythfrederiksen@gmail.com>
2022-09-01 11:15:52 +0200

    Test: Comment out all code

--- src/js/lazyfit.js
+++ src/js/lazyfit.js
@@ -1,45 +1,45 @@
-requestIdleCallback(lazyfit);
-function lazyfit() {
-    const halfWindowHeight = window.innerHeight / 2;
-    const images = [...document.getElementsByClassName("lazyfit")];
+// requestIdleCallback(lazyfit);
+// function lazyfit() {
+//     const halfWindowHeight = window.innerHeight / 2;
+//     const images = [...document.getElementsByClassName("lazyfit")];
 
-    const observer = new IntersectionObserver((entries, observer) => {
-        requestIdleCallback((IdleDeadline) => {
-            for (const entry of entries) {
-                if (entry.isIntersecting) {
-                    const offsetHeight = entry.target.offsetHeight;
-                    const offsetWidth = entry.target.offsetWidth;
-                    entry.target.style.height = `${offsetHeight}px`;
-                    entry.target.style.width = `${offsetWidth}px`;
-                    const pixelHeight = offsetHeight * window.devicePixelRatio;
-                    const pixelWidth = offsetWidth * window.devicePixelRatio;
-                    entry.target.src = entry.target.dataset.src.replace("{width}", pixelWidth).replace("{height}", pixelHeight);
-                    observer.unobserve(entry.target);
-                    entry.target.addEventListener("load", () => {
-                        if (entry.target.dataset.addClass) {
-                            for (const className of entry.target.dataset.addClass.split(" ")) {
-                                entry.target.classList.add(className);
-                            }
-                        }
-                        if (entry.target.dataset.removeClass) {
-                            for (const className of entry.target.dataset.removeClass.split(" ")) {
-                                entry.target.classList.remove(className);
-                            }
-                        }
-                        if (entry.target.dataset.toggleClass) {
-                            for (const className of entry.target.dataset.toggleClass.split(" ")) {
-                                entry.target.classList.toggle(className);
-                            }
-                        }
-                    });
-                }
-            }
-        });
-    }, {
-        rootMargin: `${halfWindowHeight}px 0px ${halfWindowHeight}px 0px`
-    });
+//     const observer = new IntersectionObserver((entries, observer) => {
+//         requestIdleCallback((IdleDeadline) => {
+//             for (const entry of entries) {
+//                 if (entry.isIntersecting) {
+//                     const offsetHeight = entry.target.offsetHeight;
+//                     const offsetWidth = entry.target.offsetWidth;
+//                     entry.target.style.height = `${offsetHeight}px`;
+//                     entry.target.style.width = `${offsetWidth}px`;
+//                     const pixelHeight = offsetHeight * window.devicePixelRatio;
+//                     const pixelWidth = offsetWidth * window.devicePixelRatio;
+//                     entry.target.src = entry.target.dataset.src.replace("{width}", pixelWidth).replace("{height}", pixelHeight);
+//                     observer.unobserve(entry.target);
+//                     entry.target.addEventListener("load", () => {
+//                         if (entry.target.dataset.addClass) {
+//                             for (const className of entry.target.dataset.addClass.split(" ")) {
+//                                 entry.target.classList.add(className);
+//                             }
+//                         }
+//                         if (entry.target.dataset.removeClass) {
+//                             for (const className of entry.target.dataset.removeClass.split(" ")) {
+//                                 entry.target.classList.remove(className);
+//                             }
+//                         }
+//                         if (entry.target.dataset.toggleClass) {
+//                             for (const className of entry.target.dataset.toggleClass.split(" ")) {
+//                                 entry.target.classList.toggle(className);
+//                             }
+//                         }
+//                     });
+//                 }
+//             }
+//         });
+//     }, {
+//         rootMargin: `${halfWindowHeight}px 0px ${halfWindowHeight}px 0px`
+//     });
 
-    for (const image of images) {
-        observer.observe(image);
-    }
-}
+//     for (const image of images) {
+//         observer.observe(image);
+//     }
+// }
