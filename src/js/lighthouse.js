119277a98be69f76b7926c62c26811221c751d29
DLFDK <dennislythfrederiksen@gmail.com>
2022-09-04 13:20:56 +0200

    Delete

--- src/js/lighthouse.js
+++
@@ -1,9 +0,0 @@
-// console.log("Hello");
-// lighthouse();
-async function lighthouse(){
-    const apiKey = "AIzaSyBHIHk-588ybweDebHePyn-oMyVmQUe1xU"
-    const result = await fetch(`https://www.googleapis.com/pagespeedonline/v5/runPagespeed?key=${apiKey}&url=https://www.mittinyhouse.dk/&strategy=mobile`).then(response => response.json());
-    console.log(result);
-}
-
-// console.log(document.getElementById("perf-phone__ring-overlay").getTotalLength());
