5cbf0ce55ecf08c6754988d1cbf9bf0b50f4b8c0
DLFDK <dennislythfrederiksen@gmail.com>
2022-09-16 04:31:48 +0200

    SImplify using ternaries

--- src/js/lazyfit.js
+++ src/js/lazyfit.js
@@ -7,47 +7,24 @@ if (window.requestIdleCallback) {
 function lazyfit() {
     const halfWindowHeight = window.innerHeight / 2;
     const images = [...document.getElementsByClassName("lazyfit")];
-
     const observer = new IntersectionObserver((entries, observer) => {
         for (const entry of entries) {
             if (entry.isIntersecting) {
                 observer.unobserve(entry.target);
 
-                // const newVar = entry.target.dataset.parent ? entry.target.parentElement.offsetHeight : entry.target.offsetHeight;
+                const targetParent = entry.target.dataset.parent;
+                const offsetHeight = targetParent ? entry.target.parentElement.offsetHeight : entry.target.offsetHeight;
+                const offsetWidth = targetParent ? entry.target.parentElement.offsetWidth : entry.target.offsetWidth;
 
-                let offsetHeight;
-                let offsetWidth
-                if (entry.target.dataset.parent) {
-                    offsetHeight = entry.target.parentElement.offsetHeight;
-                    offsetWidth = entry.target.parentElement.offsetWidth;
-                } else {
-                    offsetHeight = entry.target.offsetHeight;
-                    offsetWidth = entry.target.offsetWidth;
-                }
                 if (entry.target.dataset.round) {
                     entry.target.style.height = `${offsetHeight}px`;
                     entry.target.style.width = `${offsetWidth}px`;
                 }
-                // switch (isNaN(maxDPR) || maxDPR) {
-                //     case true:
-                //         maxDPR = window.devicePixelRatio;
-                //         break;
-                //     case "0":
-                //         maxDPR = 1;
-                //         break;
-                //     default:
-                //         maxDPR = Math.min(maxDPR, window.devicePixelRatio)
-                // }
-                const maxDPR = entry.target.dataset.maxDpr ? Math.min(entry.target.dataset.maxDpr, window.devicePixelRatio) : window.devicePixelRatio;
-                // if (maxDPR) {
-                //     maxDPR = Math.min(maxDPR, window.devicePixelRatio)
-                // } else {
-                //     maxDPR = window.devicePixelRatio;
-                // }
-                console.log(maxDPR);
-                const pixelHeight = offsetHeight * maxDPR;
-                const pixelWidth = offsetWidth * maxDPR;
-                entry.target.src = entry.target.dataset.src.replace("{width}", pixelWidth).replace("{height}", pixelHeight);
+
+                const maxDPR = entry.target.dataset.maxDpr;
+                const DPR = maxDPR ? Math.min(maxDPR, window.devicePixelRatio) : window.devicePixelRatio;
+                
+                entry.target.src = entry.target.dataset.src.replace("{width}", offsetWidth * DPR).replace("{height}", offsetHeight * DPR);
 
                 entry.target.addEventListener("load", () => {
                     if (entry.target.dataset.addClass) {
